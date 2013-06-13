34e48f37f866572c9b1118e123951ef1e3484b05
manojgudi <manoj.p.gudi@gmail.com>
2013-06-13 16:55:25 +0530

    returns function names used in script(removes false positives like array_names and names from comments)

--- check.py
+++ check.py
@@ -1,23 +1,74 @@
 import os
 import subprocess
+import re
 
-os.getcwd()
 
-'''
-def append_exit(absolute_file):
-	file_obj = open(absolute_file,'r+w')
-	content = file_obj.read()
-	content += '\n exit'
-	file_obj.close()
-'''
+def list2str(list_data):
+	formatted_string = ""
+	for i in list_data:
+		formatted_string = formatted_string + i + "\n"
+	return formatted_string
 
-for roots, dirs, files in os.walk('/media/d2c1960d-a6c5-4a50-a642-1cd33212cde0/uploads'):
+def rm_comments(content):
+	list_data = content.splitlines()
+	content_void_comments = []
+	for i in list_data:
+		if i.count("//") != 0:
+			i = i.split("//")[0]
+		content_void_comments.append(i)
+	return list2str(content_void_comments)		
+
+# func_name is raw with "("
+def isArray(func_name, content):
+	func_name = func_name.strip("(")
+	# Finds =>  'name('  'name ' 'name'
+	all_func_invoked = re.findall(func_name+'[ =\(]', content)
+	count = len(all_func_invoked)
+	#print "all func" +str(all_func_invoked)
+	
+	for func_invoked in all_func_invoked:
+		if func_invoked.find("(") == -1:
+			count -= 1
+		else:
+			pass
+	if count != len(all_func_invoked):
+		#print "ARRAY "+func_name	
+		pass
+	else:
+		print "FUNC: "+str(all_func_invoked) 
+
+def extract_func(abs_file):
+	file_obj = open(abs_file, 'r')
+	file_content = rm_comments(file_obj.read())
+	list_of_func = re.findall("[\w]+\(", file_content)
+	
+	if list_of_func == []:
+		return 0
+	def rm_loops(list_of_func, content):
+		proper_func_list = []
+		for func in list_of_func:
+			if ((func != 'if') and (func != 'for') and (func != 'while') and (isArray(func,content))): # Replace last condition by array condition
+				func = func.strip("(")
+				proper_func_list.append(func)
+
+		return proper_func_list
+
+	list_of_func = rm_loops(list_of_func, file_content)
+	return list_of_func
+
+path='/media/d2c1960d-a6c5-4a50-a642-1cd33212cde0/uploads/45/CH7/EX7.4/'
+#path='/media/d2c1960d-a6c5-4a50-a642-1cd33212cde0/uploads'
+for roots, dirs, files in os.walk(path):
 	for scifile in files:
 		if (scifile.find('.sci') != -1) or (scifile.find('.sce') !=-1 ):
-			#print(roots+'/'+scifile)
 			fullfile = roots+'/'+scifile
 			print fullfile
-			list(fullfile)
-			subprocess.call(['./yelp', fullfile])
-			#append_exit(fullfile)
+			(extract_func(fullfile))
+			# Call yelp
+			#subprocess.call(['./yelp', fullfile])
 
+#argparse files
+# remove comments from content before parsing
+# remove array from content, my_array(5) 5 => index
+# integrate yelp
+# printf( N(, N(, N(  case
