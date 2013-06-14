dfa38731445ed753e753f9bfd6beb8ffd68ae7fb
manojgudi <manoj.p.gudi@gmail.com>
2013-06-14 14:33:27 +0530

    returns list of functions which are less likely to be false positives | added Srikant's script: dl all funcs of scilab version

--- check.py
+++ check.py
@@ -21,41 +21,48 @@ def rm_comments(content):
 # func_name is raw with "("
 def isArray(func_name, content):
 	func_name = func_name.strip("(")
+	
 	# Finds =>  'name('  'name ' 'name'
 	all_func_invoked = re.findall(func_name+'[ =\(]', content)
 	count = len(all_func_invoked)
-	#print "all func" +str(all_func_invoked)
-	
 	for func_invoked in all_func_invoked:
 		if func_invoked.find("(") == -1:
 			count -= 1
 		else:
 			pass
+	
+	# Yes, the func_name is actually an array
 	if count != len(all_func_invoked):
-		#print "ARRAY "+func_name	
-		pass
+	#	print "FUNCNAME" + func_name
+		return True
+
 	else:
-		print "FUNC: "+str(all_func_invoked) 
+		return False
 
 def extract_func(abs_file):
 	file_obj = open(abs_file, 'r')
 	file_content = rm_comments(file_obj.read())
 	list_of_func = re.findall("[\w]+\(", file_content)
 	
-	if list_of_func == []:
-		return 0
+	# Function to remove false positive loop keywords like for/while and array declarations
 	def rm_loops(list_of_func, content):
 		proper_func_list = []
 		for func in list_of_func:
-			if ((func != 'if') and (func != 'for') and (func != 'while') and (isArray(func,content))): # Replace last condition by array condition
+			if ((func != 'if') and (func != 'for') and (func != 'while') and (not(isArray(func,content)))): # Replace last condition by array condition
 				func = func.strip("(")
 				proper_func_list.append(func)
 
 		return proper_func_list
+	
+	# If no functions are used
+	if list_of_func == []:
+		return 0
 
-	list_of_func = rm_loops(list_of_func, file_content)
+	list_of_func = list(set(rm_loops(list_of_func, file_content)))
 	return list_of_func
 
+
+# MAIN LOOP FOR TRAVERSING
 path='/media/d2c1960d-a6c5-4a50-a642-1cd33212cde0/uploads/45/CH7/EX7.4/'
 #path='/media/d2c1960d-a6c5-4a50-a642-1cd33212cde0/uploads'
 for roots, dirs, files in os.walk(path):
@@ -68,7 +75,9 @@ for roots, dirs, files in os.walk(path):
 			#subprocess.call(['./yelp', fullfile])
 
 #argparse files
-# remove comments from content before parsing
+# remove comments from content before parsing DONE
 # remove array from content, my_array(5) 5 => index
 # integrate yelp
 # printf( N(, N(, N(  case
+# remove user funcions
+
