8df214e102d342ca697bb56bc270ce0525022d89
manojgudi <manoj.p.gudi@gmail.com>
2013-06-17 14:55:01 +0530

    segregates all functions in compatible/non-compatible/user

--- check.py
+++ check.py
@@ -64,24 +64,64 @@ def extract_func(abs_file):
 	return list_of_func
 
 
+def inFuncList(func_name, func_list_file):
+	# Code To open func_list_file with first letter of func_name
+	try:
+		file_obj = open(func_list_file,'r')
+		func_list = file_obj.read().split('\n')
+		file_obj.close()
+		
+		if func_list.count(func_name) == 1:
+			return True
+		else:
+			return False
+	except:
+		# If no open(func_list_file) throws error like in case of 1.txt
+		return False
 
-def inFiveThreeThree(func_name, func_list_file):
-	file_obj = open(func_list_file,'r')
-	func_list = file_obj.read().split('\n')
-	file_obj.close()
 
+
+# Is the func_name found in func_list_file of 5.3.3 version
+def isCompatible(func_name):
+	
+	# Check for scilab 5.3.3 => base version
+	# Code To open func_list_file with first letter of func_name
+	func_list_file = 'sci_five_three_three/'+func_name[0].lower() + '.txt'
+
+	if inFuncList(func_name, func_list_file):
+		# Check For other versions
+		func_list_file = func_name[0].lower()+'.txt'
+		if inFuncList(func_name, func_list_file):
+			return 0
+		else:
+			# Non-compatible Function
+			return 1
+	else:
+		# func_name is either User Function or False Positive
+		return -1
 	
 
 # MAIN LOOP FOR TRAVERSING
+#path='/media/d2c1960d-a6c5-4a50-a642-1cd33212cde0/uploads/51/CH12/EX12.2/12_2data.sci'
 #path='/media/d2c1960d-a6c5-4a50-a642-1cd33212cde0/uploads/45/CH7/EX7.4/'
-#path='/media/d2c1960d-a6c5-4a50-a642-1cd33212cde0/uploads'
-path = 'dummy_scripts'
+path='/media/d2c1960d-a6c5-4a50-a642-1cd33212cde0/uploads'
+#path = 'dummy_scripts'
 for roots, dirs, files in os.walk(path):
 	for scifile in files:
 		if (scifile.find('.sci') != -1) or (scifile.find('.sce') !=-1 ):
 			fullfile = roots+'/'+scifile
 			print fullfile
-			print str(extract_func(fullfile))
+			func_list = (extract_func(fullfile))
+			
+			if func_list != 0:
+				for func_name in func_list:
+					return_value = isCompatible(func_name)
+					if return_value == 0 :
+						print 'Function Compatible: ', func_name, '\n' 
+					elif return_value == 1:
+						print 'Function not compatible: ', func_name, '\n'
+					else:
+						print "User Function: ", func_name,'\n'
 			# Call yelp
 			#subprocess.call(['./yelp', fullfile])
 
